d1f5a8238f1d830b1cb21b060014dc29e5b8a2c1
Gustavo Lopes <guhrlopes999@gmail.com>
2026-01-29 09:39:11 -0300

    Adicionado funcionlidade de impressão no botão

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("submit-button").addEventListener("submit", () => {
+  console.log(document.getElementById("name").value)
+  console.log(document.getElementById("birth-date").value)
+})
+
+
